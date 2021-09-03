5d9be3c9e0c4c9356e74617bfe29191429f4125d
Tim Pillinger <26465611+wxtim@users.noreply.github.com>
2021-09-03 15:07:26 +0100

    foo

--- src/components/BookForm.vue
+++ src/components/BookForm.vue
@@ -2,12 +2,15 @@
   <form v-on:submit.prevent="bookSubmit(bookTitle, bookAuthor)" class="" action="#" method="post">
     <input v-model="bookData.bookTitle" type="text" name="title" value="" placeholder="Book Title">
     <input v-model="bookData.bookAuthor" type="text" name="author" value="" placeholder="Book Author">
-    <input v-model="bookData.finishedReading" type="checkbox" name="finished">
-    <label for="bookData.finishedReading" text="Finished Reading"></label>
-    <input v-model="bookData.ownership" type="radio" value="borrowed">
-    <label for="borrowed" text="borrowed"></label>
-    <input v-model="bookData.ownership" type="radio" value="brought">
-    <label for="bought" text="bought"></label>
+    <label for="bookData.finishedReading" text="Finished Reading">
+      <input v-model="bookData.finishedReading" type="checkbox" name="finished">
+    </label>
+    <label for="borrowed" text="borrowed">
+      <input v-model="bookData.ownership" type="radio" value="borrowed">
+    </label>
+    <label for="bought" text="bought">
+      <input v-model="bookData.ownership" type="radio" value="brought">
+    </label>
 
     <button type="submit" name="button">Add Book</button>
   </form>
